e851f4873db9d4eb69a37bdc5c08672b8f67f7ee
Pragati Chothe <pragatichothe@gmail.com>
2023-06-26 18:17:39 +0530

    SQL query to fetch data.

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
+const express = require("express");
+const connection = require("./database/db-query");
+const app = express();
+const port = 4000;
 
-const express = require('express')
-const connection = require('./database/db-query')
-const app = express()
-const port = 4000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-  
-})
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM event_table", (error, results, fields) => {
+    if (error) {
+      console.error("Error executing the query:", error);
+      return;
+    }
+    console.log("Result:", results);
+    res.json({ results });
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-  //const result = connection.query('SELECT * FROM event_table')
-  //console.log(result, 'table from event')
-  connection.query(
-    'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
-    function(err, results, fields) {
-      console.log(results); // results contains rows returned by server
-      console.log(fields); // fields contains extra meta data about results, if available
-    }
-  );
-})
+  console.log(`Example app listening on port ${port}`);
+});
