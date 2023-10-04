98c327fc298ced4fa1dadc1bcb71aec9bb7d2a61
Pragati Chothe <pragatichothe@gmail.com>
2023-10-04 21:53:22 +0530

    registerUsers, loginUsers both work successfully

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -6,17 +6,19 @@ const connection = require("../database/db-connection");
 
 //User Registration
 const registerUsers = (req, res) => {
-    const { userID, userName, emailID, profilePic, created_at, updated_at } = req.body;
+    const { userID, userName, emailID, password, profilePic, created_at, updated_at } = req.body;
 
     // Insert user data into the MySQL database
-    const registerQuery = 'INSERT INTO user_table (userID, userName, emailID, profilePic, created_at, updated_at) VALUES (?, ?, ?, ?, NOW(), NOW())';
-    connection.query(registerQuery, [userID, userName, emailID, profilePic, created_at, updated_at], (error, results) => {
+    const registerQuery = 'INSERT INTO user_table (userID, userName, emailID, password, profilePic, created_at, updated_at) VALUES (?, ?, ?, ?, ?, NOW(), NOW())';
+    connection.query(registerQuery, [userID, userName, emailID, password, profilePic, created_at, updated_at], (error, results) => {
       if (error) {
         console.error('Error registering user in MySQL:', error);
-        res.status(500).json({ error: 'Registration failed' });
+        //Internal Server Error
+        res.status(500).json({ error: '500 : Registration failed' });
       } else {
-        res.status(201).json({ message: 'User registered successfully' });
+        res.status(201).json({ message: '200 : User registered successfully' });
       }
+      console.log("[INFO] registerQuery: ", results);
     });
 };
 
@@ -35,7 +37,7 @@ const loginUsers = (req, res) => {
     } else {
       if (results.length === 0) {
         // User with the provided email not found
-        res.status(404).json({ error: 'User not found' });
+        res.status(404).json({ error: '404 : User not found' });
       } else {
         // User found, check password
         const user = results[0];
@@ -43,10 +45,10 @@ const loginUsers = (req, res) => {
         // Compare the provided password with the stored password (plaintext)
         if (password === user.password) {
           // Passwords match, user is authenticated
-          res.status(200).json({ message: 'Login successful', user });
+          res.status(200).json({ message: '200 : Login successful', user });
         } else {
-          // Passwords do not match
-          res.status(401).json({ error: 'Authentication failed' });
+          // Passwords do not match : Authentication failed
+          res.status(401).json({ error: '401 : Passwords do not match' });
         }
       }
     }
