bbb1c7197112aedc7c54b1f79720ebf87f992438
Pragati Chothe <pragatichothe@gmail.com>
2023-10-04 12:00:50 +0530

    Added URL in Comment

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 //In controller we have write request and response
 const {createUsers} = require("../controller/user-controller");
 
-//Create a new User 
+//Create a new User http://localhost:4000/users/register
 router.post("/register", createUsers);
 
 
