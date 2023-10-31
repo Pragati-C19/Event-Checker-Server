e125f924e0e91a3736bd7c25d3b275fbe08ced05
Pragati Chothe <pragatichothe@gmail.com>
2023-10-31 21:04:16 +0530

    middleware is created

--- middleware/token-auth.js
+++ middleware/token-auth.js
@@ -1,11 +1,23 @@
-
 function authenticateToken(req, res, next) {
-    // Check if the user is authenticated
-    if (userIsAuthenticated) {
-      next(); // Continue to the next middleware or route handler
-    } else {
-      res.status(401).send('Unauthorized');
-    }
+  /*
+    Token is going to come from header
+    authorization header format is "Bearer TOKEN"
+
+ */
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; //if we have authHeader then return authHeader token portion we split on the space otherwise just return undefined
+
+  // Check if the user is authenticated
+  if (token==null) {
+    res.status(401).send("Unauthorized");
+  } else {
+    //if we have a token we need to verify that token 
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if(err) return res.status(403).send("You Don't have access")
+        req.user=user;
+        next(); // Continue to the next middleware or route handler
+    });
+  }
 }
 
-module.exports = {authenticateToken}
+module.exports = { authenticateToken };
