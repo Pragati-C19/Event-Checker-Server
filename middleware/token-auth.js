398bd6d310b08983ba62b65b089710e7f2d2881f
Pragati Chothe <pragatichothe@gmail.com>
2023-11-09 20:55:44 +0530

    Now we are NOT using Beare token

--- middleware/token-auth.js
+++ middleware/token-auth.js
@@ -1,21 +1,27 @@
+const jwt = require("jsonwebtoken");
+
 function authenticateToken(req, res, next) {
   /*
-    Token is going to come from header
-    authorization header format is "Bearer TOKEN"
+    Token is going to come from header(Authorization)
+    authorization header format was "Bearer TOKEN"
+    We can directly use token instead of adding Bearer
  */
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1]; //if we have authHeader then return authHeader token portion we split on the space otherwise just return undefined
+  const token = authHeader;
+
+  const emptyValues = ["", null, undefined];
 
   // Check if the user is authenticated
-  if (token==null) {
-    res.status(401).send("Unauthorized");
+  if (emptyValues.includes(token)) {
+    //TODO Change all status code format as this one
+    res.status(200).json({ statusCode: 401, statusMsg: "Unauthorized" });
   } else {
-    //if we have a token we need to verify that token 
-    //this user_id is used in mysql table 
+    //if we have a token we need to verify that token
+    //this user_id is used in mysql table
     jwt.verify(token, process.env.JWT_SECRET, (err, user_id) => {
-        if(err) return res.status(403).send("You Don't have access")
-        req.user_id=user_id;
-        next(); // Continue to the next middleware or route handler
+      if (err) return res.status(403).send("You Don't have access");
+      req.user_id = user_id;
+      next(); // Continue to the next middleware or route handler
     });
   }
 }
